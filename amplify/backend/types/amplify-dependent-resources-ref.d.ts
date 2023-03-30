8b400d291a5602bcea7d595aa13b5cbf821de44b
abi abi <alibinapi@gmail.com>
2023-03-30 15:41:31 +0200

    merge dev to prod

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,18 +2,8 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "aliyouawsproject": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string"
-    }
-  },
-  "auth": {
-    "aliyouawsproject899a2898": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
     }
   }
 }
